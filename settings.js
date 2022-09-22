6a8fb9469928af30cf9ea7155f9228fc2db44b60
tharindubandara1234 <114164928+tharindubandara1234@users.noreply.github.com>
2022-09-22 15:20:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,25 +36,25 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['94764327367'] //ur owner number
+global.ownername = "Tʜᴀʀɪɴᴅᴜ 亗" //ur owner name
+global.ytname = "YT: THARINDU GAMING" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['94764327367']
+global.ownertag = '94764327367' //ur tag number
+global.botnumber = '94764327367' //Add your Bot Number For Log
+global.botname = 'Tʜᴀʀɪɴᴅᴜ 亗 BY EDITION' //ur bot name
+global.ig = "https://youtube.com/channel/UCo9s_w4q3Y938VtbBU8cRlw" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtube.com/channel/UCo9s_w4q3Y938VtbBU8cRlw" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
+global.packname = "Tʜᴀʀɪɴᴅᴜ 亗" //ur sticker watermark packname
+global.author = "Tʜᴀʀɪɴᴅᴜ 亗" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
